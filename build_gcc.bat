1121d1f0d2d2d2a69f7eed8c5098de50a0a21f60
MayaStudentPwr <281160@student.pwr.edu.pl>
2026-06-14 01:08:55 +0200

    Cleaning compiler configuration

--- build_gcc.bat
+++ build_gcc.bat
@@ -1,14 +1,13 @@
 @echo off
-echo Rozpoczynam kompilacje za pomoca GCC (MinGW)...
+echo Rozpoczynam kompilacje
 
-:: Opcje kompilatora (C++17)
 set CXX_FLAGS=-std=c++17 -Wall -O2
 
-:: Sciezki do folderow vcpkg (wersja dla MinGW)
+:: Sciezki do folderow vcpkg
 set INCLUDES=-I".\vcpkg\installed\x64-mingw-dynamic\include"
 set LIB_PATHS=-L".\vcpkg\installed\x64-mingw-dynamic\lib"
 
-:: Biblioteki do zlinkowania (kolejnosc ma znaczenie w GCC!)
+:: Biblioteki do zlinkowania
 set LIBS=-limgui -lglfw3dll -lopengl32 -lgdi32 -luser32 -lshell32
 
 :: Wywolanie kompilatora
@@ -16,7 +15,7 @@ g++ %CXX_FLAGS% main.cpp Board.cpp AI.cpp GameApp.cpp %INCLUDES% %LIB_PATHS% %LI
 
 if %ERRORLEVEL% EQU 0 (
     echo.
-    echo Sukces! Przygotowuje wymagane biblioteki runtime...
+    echo Sukces! Przygotowuje wymagane biblioteki runtime
     set "VCPKG_BIN=%~dp0vcpkg\installed\x64-mingw-dynamic\bin"
     set "MINGW_BIN=C:\msys64\mingw64\bin"
     set "PATH=%VCPKG_BIN%;%MINGW_BIN%;%PATH%"
@@ -25,7 +24,7 @@ if %ERRORLEVEL% EQU 0 (
     if exist "%MINGW_BIN%\libstdc++-6.dll" copy /y "%MINGW_BIN%\libstdc++-6.dll" "%~dp0" >nul
     if exist "%MINGW_BIN%\libwinpthread-1.dll" copy /y "%MINGW_BIN%\libwinpthread-1.dll" "%~dp0" >nul
     echo.
-    echo Sukces! Uruchamianie gry...
+    echo Sukces! Uruchamianie gry
     GraMinimax.exe
 ) else (
     echo.
